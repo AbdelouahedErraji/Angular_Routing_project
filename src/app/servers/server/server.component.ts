f84a623a43f0dceda5bab841c376137c168e5ddf
Abdelouahed Erraji <aerraji@leyton.com>
2024-06-13 16:04:37 +0100

    first commit

---
+++ src/app/servers/server/server.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+
+import { ServersService } from '../servers.service';
+import { ActivatedRoute, Data, Params, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+  styleUrls: ['./server.component.css'],
+})
+export class ServerComponent implements OnInit {
+  server: { id: number; name: string; status: string };
+  allowEdit = '';
+
+  constructor(
+    private serversService: ServersService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  ngOnInit() {
+    this.route.data.subscribe((data: Data) => {
+      this.server = data['server'];
+    });
+
+    this.route.queryParams.subscribe((queryParams: Params) => {
+      this.allowEdit = queryParams['allowEdit'];
+    });
+
+    // const id = +this.route.snapshot.params['id'];
+    // this.server = this.serversService.getServer(id);
+    // this.allowEdit = this.route.snapshot.queryParams['allowEdit'];
+    // this.route.params.subscribe((params: Params) => {
+    //   this.server = this.serversService.getServer(+params['id']);
+    // });
+    // this.route.queryParams.subscribe((queryParams: Params) => {
+    //   this.allowEdit = queryParams['allowEdit'];
+    // });
+  }
+
+  onEdit() {
+    this.router.navigate(['edit'], {
+      relativeTo: this.route,
+      queryParamsHandling: 'preserve',
+    });
+  }
+}
